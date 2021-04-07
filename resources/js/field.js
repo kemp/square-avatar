5d9b3d1327d3e873e0a7f17229f8acb90e75e047
Steven Kemp <steven@appliedimagination.com>
2021-04-07 13:50:21 -0400

    Remove unused files

--- resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  Vue.component('index-square-avatar', require('./components/IndexField'))
-  Vue.component('detail-square-avatar', require('./components/DetailField'))
-  Vue.component('form-square-avatar', require('./components/FormField'))
-})
